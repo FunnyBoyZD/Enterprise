30d26672f4e8dbb623bdc2981f09c5d90b9f6f4b
FunnyBoyZD <58341926+FunnyBoyZD@users.noreply.github.com>
2022-05-14 18:25:42 +0300

    Update ООП_3.cpp

--- ООП_3.cpp
+++ ООП_3.cpp
@@ -11,23 +11,24 @@ int main(void)
 	SetConsoleCP(CP_UTF8);
 	SetConsoleOutputCP(CP_UTF8);
 
+	cout << "===Перша частина демонстрації роботи програми:===" << endl;
 	AdministrativeAssistant AA1_1, AA1_2, AA2_1, AA2_2;
 	MarketingSpecialist MS1_1, MS1_2, MS2_1, MS2_2;
 	BusinessAnalyst BA1_1, BA1_2, BA2_1, BA2_2;
 
-	AA1_1.Set("Anastasia", 30, 10, 30000, true);
-	AA1_2.Set("Elisaveta", 40, 15, 32000, true);
-	BA1_1.Set("Galina", 20, 2, 31000, false);
-	BA1_2.Set("Stephania", 36, 18, 48000, false);
-	MS1_1.Set("Elvira", 28, 10, 37000, true);
-	MS1_2.Set("Veronika", 25, 5, 34000, false);
+	AA1_1.Set("Анастасія", 30, 10, 30000, true);
+	AA1_2.Set("Єлизавета", 40, 15, 32000, true);
+	BA1_1.Set("Галина", 20, 2, 31000, false);
+	BA1_2.Set("Стефанія", 36, 18, 48000, false);
+	MS1_1.Set("Ельвіра", 28, 10, 37000, true);
+	MS1_2.Set("Вероніка", 25, 5, 34000, false);
 
-	AA2_1.Set("Georgiy", 40, 20, 50000, true);
-	AA2_2.Set("Vitaliy", 20, 1, 32000, true);
-	BA2_1.Set("Oleksii", 24, 3, 45000, false);
-	BA2_2.Set("Oleksandr", 38, 19, 65000, false);
-	MS2_1.Set("Serhiy", 19, 1, 20000, false);
-	MS2_2.Set("Svyatoslav", 26, 7, 34000, true);
+	AA2_1.Set("Георгій", 40, 20, 50000, true);
+	AA2_2.Set("Віталій", 20, 1, 32000, true);
+	BA2_1.Set("Олексій", 24, 3, 45000, false);
+	BA2_2.Set("Олександр", 38, 19, 65000, false);
+	MS2_1.Set("Сергій", 19, 1, 20000, false);
+	MS2_2.Set("Святослав", 26, 7, 34000, true);
 
 	vector<AdministrativeAssistant> vAA1;
 	vAA1.emplace_back(AA1_1);
@@ -51,24 +52,77 @@ int main(void)
 
 	Department dep1, dep2;
 	vector<Department> items;
-	dep1.Set(vAA1, vBA1, vMS1, "Marketing");
-	dep2.Set(vAA2, vBA2, vMS2, "Finance");
+	dep1.Set(vAA1, vBA1, vMS1, "Маркетинг");
+	dep2.Set(vAA2, vBA2, vMS2, "Фінанси");
 	items.push_back(dep1);
 	items.push_back(dep2);
 	Date createdAt;
 	createdAt.Set("2000", "10", 10);
 	string name = "NVIDIA Corp.";
-	string owner = "Jen-Hsun Huang";
+	string owner = "Джен-Хсун Хуанг";
 	double proceeds = 7640000000;
 	try
 	{
 		Enterprise enterprise(items, createdAt,name,owner,proceeds);
+
+		cout << "===Корпорація до додавання нового відділу та робітників:===" << endl;
+		enterprise.Show();
+
+		Department dep3;
+		AdministrativeAssistant AA3_1, AA3_2, AA4_1, AA4_2;
+		MarketingSpecialist MS3_1, MS3_2, MS4_1, MS4_2;
+		BusinessAnalyst BA3_1, BA3_2, BA4_1, BA4_2;
+
+		AA3_1.Set("Тетяна", 30, 10, 30000, true);
+		AA3_2.Set("Ірина", 40, 15, 32000, true);
+		BA3_1.Set("Наталья", 20, 2, 31000, false);
+		BA3_2.Set("Джесіка", 36, 18, 48000, false);
+		MS3_1.Set("Ярослава", 28, 10, 37000, true);
+		MS3_2.Set("Яворина", 25, 5, 34000, false);
+
+		AA4_1.Set("Джордж", 40, 20, 50000, true);
+		AA4_2.Set("Джек", 20, 1, 32000, true);
+		BA4_1.Set("Дмитро", 24, 3, 45000, false);
+		BA4_2.Set("Стефан", 38, 19, 65000, false);
+		MS4_1.Set("Джозеф", 19, 1, 20000, false);
+		MS4_2.Set("Зигмунд", 26, 7, 34000, true);
+
+		vector<AdministrativeAssistant> vAA3;
+		vAA3.emplace_back(AA3_1);
+		vAA3.emplace_back(AA3_2);
+		vector<BusinessAnalyst> vBA3;
+		vBA3.emplace_back(BA3_1);
+		vBA3.emplace_back(BA3_2);
+		vector<MarketingSpecialist> vMS3;
+		vMS3.emplace_back(MS3_1);
+		vMS3.emplace_back(MS3_2);
+
+		vector<AdministrativeAssistant> vAA4;
+		vAA4.emplace_back(AA4_1);
+		vAA4.emplace_back(AA4_2);
+		vector<BusinessAnalyst> vBA4;
+		vBA4.emplace_back(BA4_1);
+		vBA4.emplace_back(BA4_2);
+		vector<MarketingSpecialist> vMS4;
+		vMS4.emplace_back(MS4_1);
+		vMS4.emplace_back(MS4_2);
+		
+		BusinessAnalyst newBA;
+		newBA.Set("Ігор", 26, 4, 44000, false);
+		AdministrativeAssistant newAA;
+		newAA.Set("Хрістіна", 23, 1, 21000, true);
+		MarketingSpecialist newMS;
+		newMS.Set("Майкл", 47, 23, 67000, false);
+		enterprise.GetDepartment(1)->InsertABA(&newBA);
+		enterprise.GetDepartment(1)->InsertAnAA(&newAA);
+		enterprise.GetDepartment(1)->InsertAMS(&newMS);
+
+		cout << "===Корпорація після додавання нового відділу та робітників:===" << endl;
 		enterprise.Show();
 	}
 	catch (const std::exception& ex)
 	{
 		cout << ex.what() << endl;
 	}
-	_getch();
 	return 0;
 }
