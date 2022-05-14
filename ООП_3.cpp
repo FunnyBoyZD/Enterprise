a6112adae9740fa3ff9907362d97b077c8cf11a5
FunnyBoyZD <58341926+FunnyBoyZD@users.noreply.github.com>
2022-05-14 18:37:38 +0300

    Update ООП_3.cpp

--- ООП_3.cpp
+++ ООП_3.cpp
@@ -11,58 +11,59 @@ int main(void)
 	SetConsoleCP(CP_UTF8);
 	SetConsoleOutputCP(CP_UTF8);
 
-	cout << "===Перша частина демонстрації роботи програми:===" << endl;
-	AdministrativeAssistant AA1_1, AA1_2, AA2_1, AA2_2;
-	MarketingSpecialist MS1_1, MS1_2, MS2_1, MS2_2;
-	BusinessAnalyst BA1_1, BA1_2, BA2_1, BA2_2;
-
-	AA1_1.Set("Анастасія", 30, 10, 30000, true);
-	AA1_2.Set("Єлизавета", 40, 15, 32000, true);
-	BA1_1.Set("Галина", 20, 2, 31000, false);
-	BA1_2.Set("Стефанія", 36, 18, 48000, false);
-	MS1_1.Set("Ельвіра", 28, 10, 37000, true);
-	MS1_2.Set("Вероніка", 25, 5, 34000, false);
-
-	AA2_1.Set("Георгій", 40, 20, 50000, true);
-	AA2_2.Set("Віталій", 20, 1, 32000, true);
-	BA2_1.Set("Олексій", 24, 3, 45000, false);
-	BA2_2.Set("Олександр", 38, 19, 65000, false);
-	MS2_1.Set("Сергій", 19, 1, 20000, false);
-	MS2_2.Set("Святослав", 26, 7, 34000, true);
-
-	vector<AdministrativeAssistant> vAA1;
-	vAA1.emplace_back(AA1_1);
-	vAA1.emplace_back(AA1_2);
-	vector<BusinessAnalyst> vBA1;
-	vBA1.emplace_back(BA1_1);
-	vBA1.emplace_back(BA1_2);
-	vector<MarketingSpecialist> vMS1;
-	vMS1.emplace_back(MS1_1);
-	vMS1.emplace_back(MS1_2);
-
-	vector<AdministrativeAssistant> vAA2;
-	vAA2.emplace_back(AA2_1);
-	vAA2.emplace_back(AA2_2);
-	vector<BusinessAnalyst> vBA2;
-	vBA2.emplace_back(BA2_1);
-	vBA2.emplace_back(BA2_2);
-	vector<MarketingSpecialist> vMS2;
-	vMS2.emplace_back(MS2_1);
-	vMS2.emplace_back(MS2_2);
-
-	Department dep1, dep2;
-	vector<Department> items;
-	dep1.Set(vAA1, vBA1, vMS1, "Маркетинг");
-	dep2.Set(vAA2, vBA2, vMS2, "Фінанси");
-	items.push_back(dep1);
-	items.push_back(dep2);
-	Date createdAt;
-	createdAt.Set("2000", "10", 10);
-	string name = "NVIDIA Corp.";
-	string owner = "Джен-Хсун Хуанг";
-	double proceeds = 7640000000;
 	try
 	{
+		cout << "===Перша частина демонстрації роботи програми:===" << endl;
+		AdministrativeAssistant AA1_1, AA1_2, AA2_1, AA2_2;
+		MarketingSpecialist MS1_1, MS1_2, MS2_1, MS2_2;
+		BusinessAnalyst BA1_1, BA1_2, BA2_1, BA2_2;
+
+		AA1_1.Set("Анастасія", 30, 10, 30000, true);
+		AA1_2.Set("Єлизавета", 40, 15, 32000, true);
+		BA1_1.Set("Галина", 20, 2, 31000, false);
+		BA1_2.Set("Стефанія", 36, 18, 48000, false);
+		MS1_1.Set("Ельвіра", 28, 10, 37000, true);
+		MS1_2.Set("Вероніка", 25, 5, 34000, false);
+
+		AA2_1.Set("Георгій", 40, 20, 50000, true);
+		AA2_2.Set("Віталій", 20, 1, 32000, true);
+		BA2_1.Set("Олексій", 24, 3, 45000, false);
+		BA2_2.Set("Олександр", 38, 19, 65000, false);
+		MS2_1.Set("Сергій", 19, 1, 20000, false);
+		MS2_2.Set("Святослав", 26, 7, 34000, true);
+
+		vector<AdministrativeAssistant> vAA1;
+		vAA1.emplace_back(AA1_1);
+		vAA1.emplace_back(AA1_2);
+		vector<BusinessAnalyst> vBA1;
+		vBA1.emplace_back(BA1_1);
+		vBA1.emplace_back(BA1_2);
+		vector<MarketingSpecialist> vMS1;
+		vMS1.emplace_back(MS1_1);
+		vMS1.emplace_back(MS1_2);
+
+		vector<AdministrativeAssistant> vAA2;
+		vAA2.emplace_back(AA2_1);
+		vAA2.emplace_back(AA2_2);
+		vector<BusinessAnalyst> vBA2;
+		vBA2.emplace_back(BA2_1);
+		vBA2.emplace_back(BA2_2);
+		vector<MarketingSpecialist> vMS2;
+		vMS2.emplace_back(MS2_1);
+		vMS2.emplace_back(MS2_2);
+
+		Department dep1, dep2;
+		vector<Department> items;
+		dep1.Set(vAA1, vBA1, vMS1, "Маркетинг");
+		dep2.Set(vAA2, vBA2, vMS2, "Фінанси");
+		items.push_back(dep1);
+		items.push_back(dep2);
+		Date createdAt;
+		createdAt.Set("2000", "10", 10);
+		string name = "NVIDIA Corp.";
+		string owner = "Джен-Хсун Хуанг";
+		double proceeds = 7640000000;
+
 		Enterprise enterprise(items, createdAt,name,owner,proceeds);
 
 		cout << "===Корпорація до додавання нового відділу та робітників:===" << endl;
