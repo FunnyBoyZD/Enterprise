859748478c892b442a5de7525f88434615246151
FunnyBoyZD <58341926+FunnyBoyZD@users.noreply.github.com>
2022-05-28 13:36:11 +0300

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -5,20 +5,22 @@ using namespace std;
 
 class Employee
 {
-public: //компоненти класу, до яких наданий публічний доступ	
+public: //компоненти класу, до яких наданий публічний доступ  
 	Employee(); //конструктор без параметрів
-	Employee(string name, int age, int work_experience, double payment, bool workingPlace); //конструктор з параметрами
+	Employee(string name, string profession, int age, int work_experience, double payment, bool workingPlace); //конструктор з параметрами
 	Employee(const Employee& other); //конструктор копіювання
 	Employee(Employee&&) noexcept = default;
 	Employee& operator=(Employee&&) = default;
 	Employee& operator=(const Employee&) = default; //копіювання за допомогою оператора присвоювання за замовчуванням
 	string GetName(); //геттер для імені без зазначеної кількості елементів для запису
+	string GetProfession(); //геттер для професії
 	int GetAge(); //геттер для віку
 	int GetExperience(); //геттер для стажу роботи
 	double GetPayment(); //геттер для зарплатні
 	bool GetWorkingPlace(); //геттер для місця роботи
-	void Set(string name, int age, int work_experience, double payment, bool workingPlace); //сеттер для всіх полів
+	void Set(string name, string profession, int age, int work_experience, double payment, bool workingPlace); //сеттер для всіх полів
 	void SetName(string name); //геттер для імені
+	void SetProfession(string profession); //сеттер для професії
 	void SetAge(int age); //геттер для віку
 	void SetExperience(int work_experience); //сеттер для стажу роботи
 	void SetPayment(double payment); //сеттер для зарплатні
@@ -27,6 +29,7 @@ public: //компоненти класу, до яких наданий публ
 	virtual ~Employee(); //деструктор за замовчуванням
 protected: //компоненти класу, до яких захищений доступ
 	string _name; //ім'я робітника
+	string _profession; //професія
 	int _age; //вік робітника
 	int _workExperience; //стаж роботи робітника
 	double _payment; //зарплатня
